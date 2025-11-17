f6cf87852b9b0f22062e54359cd99f9b2dd01f23
andersonsocoloski <anderson.socoloski@escola.pr.gov.br>
2025-11-17 19:30:47 -0300

    else

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ let texto = document.querySelector("#texto");
 texto.textContent = "Anderson";
 
 let nome = prompt("Qual o seu nome?");
-if (texto.textContent== null){
-    texto.textContent = "Seja Bem Vindo(a)";
+if (texto.textContent == null){
+    texto.textContent = "Seja Bem Vindo(a)!";
 }
 else {
     texto.textcontent = nome;
